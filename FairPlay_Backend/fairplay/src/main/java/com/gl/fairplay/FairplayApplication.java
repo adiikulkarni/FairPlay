e777d840044814d7df5dd528a6f440bf1467341f
Vaishvi Parikh <vaishvi.parikh@globallogic.com>
2026-04-06 14:27:55 +0530

    Initializing the project
    Initializing the main repository files

---
+++ FairPlay_Backend/fairplay/src/main/java/com/gl/fairplay/FairplayApplication.java
@@ -0,0 +1,13 @@
+package com.gl.fairplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairplayApplication.class, args);
+	}
+
+}
